2717d96b2821da5b0946ba2c8fa28e20f5c4d393
Yingyang Zhang <yingyang.zhang95@gmail.com>
2022-08-30 14:15:09 -0400

    commit tree af9fc4a1a84afca81e4a0b7a450cc1ab46ba316f

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (myArray) => {
+    return myArray.slice(0,2);
+}
+
+const returnLastTwoDrivers = (myArray) => {
+    return myArray.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(myInt) {
+    return myInt2 => {
+        return myInt2 * myInt;
+    }
+}
+
+const fareDoubler = myFare => {
+    return createFareMultiplier(2)(myFare);
+}
+
+const fareTripler = myFare => {
+    return createFareMultiplier(3)(myFare);
+}
+
+function selectDifferentDrivers(DArray, myFunction){
+    return myFunction(DArray);
+}
